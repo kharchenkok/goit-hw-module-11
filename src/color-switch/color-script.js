292db4e9cd0d99841d95b444d31168b17dc9db13
kharchenkok <k.v.kharchenko@ukr.net>
2020-08-12 22:38:07 +0300

    color-switch chngs

--- src/color-switch/color-script.js
+++ src/color-switch/color-script.js
@@ -1,10 +1,10 @@
 const refs = {
   body: document.querySelector('body'),
   buttonWrapper: document.querySelector('.button-wrapper'),
-  startBtn: document.querySelector('[data-action="start"]'),
-  stopBtn: document.querySelector('[data-action="stop"]'),
+  buttons: document.querySelectorAll('[data-action]'),
 };
 
+console.log(refs.buttons);
 const colors = [
   '#FFFFFF',
   '#2196F3',
@@ -17,26 +17,31 @@ const colors = [
 let switchInterval;
 
 const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+  return Math.floor(Math.random() * (max - min + 1) + min);
 };
 
 const switchBackground = function (e) {
   if (e.target.nodeName !== 'BUTTON') {
     return;
   }
+
   if (e.target.dataset.action === 'start') {
-    refs.startBtn.disabled = true;
-    refs.stopBtn.disabled = false;
     switchInterval = setInterval(() => {
       refs.body.style.backgroundColor =
         colors[randomIntegerFromInterval(0, colors.length - 1)];
     }, 1000);
   } else if (e.target.dataset.action === 'stop') {
-    refs.startBtn.disabled = false;
-    refs.stopBtn.disabled = true;
     clearInterval(switchInterval);
   }
+  changeBtnDisable(e.target.dataset.action);
 };
 
+function changeBtnDisable(value) {
+  return refs.buttons.forEach(button =>
+    button.dataset.action === value
+      ? (button.disabled = true)
+      : (button.disabled = false),
+  );
+}
 
 refs.buttonWrapper.addEventListener('click', switchBackground);
